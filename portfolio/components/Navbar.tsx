f465ad6ee41bb9586df78c09392f44d2ff97308c
Aaron He <aaronhe09@yahoo.com>
2023-08-24 16:35:56 -0700

    added background image island

---
+++ portfolio/components/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <head>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </head>
+  );
+};
+
+export default Navbar;
