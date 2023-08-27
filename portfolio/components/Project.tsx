e4859fa2252687195366d02c0246f3be0091ab91
Aaron He <aaronhe09@yahoo.com>
2023-08-27 16:31:45 -0700

    added preview picture to top half of card

--- portfolio/components/Project.tsx
+++ portfolio/components/Project.tsx
@@ -9,13 +9,25 @@ const Project = () => {
       {images.map((image) => {
         return (
           <article className="basis-full md:basis-1/2 mb-10">
-            <div className=" max-w-[300px] h-[400px] w-full bg-white m-auto">
-              <Image
-                src={`/${image}.webp`}
-                alt={`/${image} project`}
-                width={50}
-                height={50}
-              />
+            <div className=" max-w-[300px] h-[450px] bg-white m-auto relative">
+              <div className="h-1/2 relative">
+                <Image
+                  src={`/${image}-background.webp`}
+                  alt="background"
+                  fill={true}
+                  objectFit="cover"
+                  style={{ position: "absolute", top: 0, left: 0 }}
+                />
+              </div>
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-solid border-black border-2 p-3 rotate-45 bg-white">
+                <Image
+                  src={`/${image}.webp`}
+                  alt={`/${image} project`}
+                  width={40}
+                  height={40}
+                  className=" -rotate-45"
+                />
+              </div>
             </div>
           </article>
         );
