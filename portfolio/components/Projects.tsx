f733ec631cef061e729dbfe8017add5bd3eac74e
Aaron He <aaronhe09@yahoo.com>
2023-08-27 14:14:17 -0700

    created new section for projects and added banner

---
+++ portfolio/components/Projects.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { glow, staggerText } from "@/app/variants/variants";
+import Carousel from "./Carousel";
+
+const Projects = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("show");
+    }
+  }, [isInView]);
+
+  return (
+    <div className="section_div" ref={ref}>
+      <h2 className="h2">
+        <motion.span
+          className="section_number"
+          variants={staggerText}
+          initial={"hidden"}
+          whileInView={"show"}
+          viewport={{ once: true }}
+        >
+          02.
+        </motion.span>
+        <motion.span
+          className="section_banner"
+          variants={glow}
+          initial={"hidden"}
+          whileInView={"show"}
+          viewport={{ once: true }}
+        >
+          Projects
+        </motion.span>
+      </h2>
+    </div>
+  );
+};
+
+export default Projects;
