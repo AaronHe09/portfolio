de318d99d2315a959c8652b29d62ba1fea28b13f
Aaron He <aaronhe09@yahoo.com>
2023-08-26 17:39:40 -0700

    animation reveals when it is in view

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import About from "@/components/About";
 import Parallax from "@/components/Parallax";
 
--- portfolio/components/About.tsx
+++ portfolio/components/About.tsx
@@ -1,14 +1,26 @@
-import { motion } from "framer-motion";
+"use client";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
 import { container, glow, staggerText } from "@/app/variants/variants";
 
 const About = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("show");
+    }
+  }, [isInView]);
+
   return (
-    <div className="section_div">
+    <div className="section_div" ref={ref}>
       <motion.h2
         className="h2"
         variants={container}
         initial="hidden"
-        animate="show"
+        animate={mainControls}
         exit="hidden"
       >
         <motion.span className="section_number" variants={staggerText}>
