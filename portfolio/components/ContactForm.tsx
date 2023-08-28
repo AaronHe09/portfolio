d9ca677667d89121cdede82a3f9bcfa74d8d5cbf
Aaron He <aaronhe09@yahoo.com>
2023-08-28 13:23:17 -0700

    downloaded emailjs and created form that will send message to my email

--- portfolio/components/ContactForm.tsx
+++ portfolio/components/ContactForm.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm(
+          "service_ngo1mf7",
+          "template_xzsqkwn",
+          form.current,
+          "HGtSpJeRz61MWwuVc"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      form.current.reset();
+    }
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <button type="submit">Send</button>
+    </form>
+  );
 };
 
 export default ContactForm;
