52957f35ebf7d13a509b1d2c557266fdcfebcbb8
Aaron He <aaronhe09@yahoo.com>
2023-08-27 19:52:38 -0700

    created a css variable for light grey

--- portfolio/components/Contact.tsx
+++ portfolio/components/Contact.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { glow, staggerText } from "@/app/variants/variants";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("show");
+    }
+  }, [isInView]);
+
+  return (
+    <div className="section_div" ref={ref}>
+      <h2 className="h2">
+        <motion.span
+          className="section_number"
+          variants={staggerText}
+          initial={"hidden"}
+          whileInView={"show"}
+          viewport={{ once: true }}
+        >
+          03.
+        </motion.span>
+        <motion.span
+          className="section_banner"
+          variants={glow}
+          initial={"hidden"}
+          whileInView={"show"}
+          viewport={{ once: true }}
+        >
+          Contact
+        </motion.span>
+      </h2>
+    </div>
+  );
 };
 
 export default Contact;
