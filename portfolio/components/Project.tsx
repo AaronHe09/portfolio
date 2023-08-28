5950b22f23ec095c337e2d9354aa4099b3f03625
Aaron He <aaronhe09@yahoo.com>
2023-08-28 14:58:10 -0700

    removed animations for individual elements and wrapped it around the parent element

--- portfolio/components/Project.tsx
+++ portfolio/components/Project.tsx
@@ -81,38 +81,24 @@ const Project = () => {
               />
               <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-75 group-hover:scale-100 group-hover:bg-white/30 cursor-pointer"></div>
             </motion.div>
-            <div className="w-full border-b-2">
-              <motion.h3
-                className="text-3xl text-white text-center"
-                variants={staggerText}
-                initial={"hidden"}
-                whileInView={"show"}
-                viewport={{ once: true }}
-              >
-                {title}
-              </motion.h3>
+            <motion.div
+              className="w-full border-b-2"
+              variants={staggerText}
+              initial={"hidden"}
+              whileInView={"show"}
+              viewport={{ once: true }}
+            >
+              <h3 className="text-3xl text-white text-center">{title}</h3>
               <div className="flex">
-                <motion.div
-                  className="p-5 basis-2/3 flex flex-wrap gap-4"
-                  variants={staggerText}
-                  initial={"hidden"}
-                  whileInView={"show"}
-                  viewport={{ once: true }}
-                >
+                <div className="p-5 basis-2/3 flex flex-wrap gap-4">
                   {mapLang(lang)}
-                </motion.div>
-                <motion.div
-                  className="p-5 basis-1/3 flex gap-5"
-                  variants={staggerText}
-                  initial={"hidden"}
-                  whileInView={"show"}
-                  viewport={{ once: true }}
-                >
+                </div>
+                <div className="p-5 basis-1/3 flex gap-5">
                   {gitHubLink(github)}
                   {liveLink(live)}
-                </motion.div>
+                </div>
               </div>
-            </div>
+            </motion.div>
             <div className="mt-5">
               <motion.p
                 className="text-xl text-white"
